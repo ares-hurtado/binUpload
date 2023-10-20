6c179bac1f0316dee49105948f85567b85d72d77
mhrtado <mhurtado@arestechnologies.com>
2023-10-20 13:01:45 -0700

    Svelte Configuration

---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+    import '@picocss/pico'
+    import '../app.css'
+</script>
+
+<div class="main-container">
+    <div class="center-container">
+        <h1>Archive Creation</h1>
+        <form action="pumped-tarpon-promptly.ngrok-free.app/archive" method="POST" enctype="multipart/form-data">
+            <input type="file" name="files" multiple>
+            <button type="submit">Upload</button>
+        </form>
+    </div>
+</div>
+
+<style>
+    .main-container {
+        display: flex;
+        height: 100vh;
+        justify-content: center;
+        align-items: center;
+    }
+    .center-container h1{
+        font-size: 4rem;
+    }
+    button {
+        width: 100%;
+    }
+</style>
