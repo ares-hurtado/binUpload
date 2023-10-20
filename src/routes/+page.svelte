b19aa7f26137a099a670a372b7debe854f3894ca
mhrtado <mhurtado@arestechnologies.com>
2023-10-20 13:08:18 -0700

    Update HTTPS Request
    Forgot to include HTTPS at the start of the URL.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
 <div class="main-container">
     <div class="center-container">
         <h1>Archive Creation</h1>
-        <form action="pumped-tarpon-promptly.ngrok-free.app/archive" method="POST" enctype="multipart/form-data">
+        <form action="https://pumped-tarpon-promptly.ngrok-free.app/archive" method="POST" enctype="multipart/form-data">
             <input type="file" name="files" multiple>
             <button type="submit">Upload</button>
         </form>
